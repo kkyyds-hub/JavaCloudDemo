894739e89e48e128e2fa588fa6972c5c5eb26095
kk <kk@AE86.cn>
2025-11-03 19:56:51 +0800

    demo的一些提交

---
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.itheima.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+    void saveUser(User user);
+
+    void deleteUser(Long id);
+
+    void updateUser(User user);
+
+    User queryUserById(@Param("id") Long id);
+
+    List<User> queryUserByIds(@Param("ids") List<Long> ids);
+
+
+    User queryById(long l);
+}
